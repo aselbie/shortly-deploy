354a9df968034a726ce626acfaf53367760c7f66
Andrew Selbie <aselbie@gmail.com>
2014-10-09 11:17:54 -0700

    Refactored for Mongo

--- app/models/link.js
+++ app/models/link.js
@@ -1,19 +1,23 @@
 var db = require('../config');
 var crypto = require('crypto');
 
-var Link = db.Model.extend({
-  tableName: 'urls',
-  hasTimestamps: true,
-  defaults: {
-    visits: 0
-  },
-  initialize: function(){
-    this.on('creating', function(model, attrs, options){
-      var shasum = crypto.createHash('sha1');
-      shasum.update(model.get('url'));
-      model.set('code', shasum.digest('hex').slice(0, 5));
-    });
-  }
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+LinkSchema = new Schema({
+  url: { type: String, default: '' },
+  base_url: { type: String, default: '' },
+  code: { type: String, default: '' },
+  title: { type: String, default: '' },
+  visits: { type: Number, default: 0 }
 });
 
-module.exports = Link;
+LinkSchema.pre('save', function(next) {
+  var shasum = crypto.createHash('sha1');
+  shasum.update(this.url);
+  this.code = shasum.digest('hex').slice(0, 5);
+  next();
+});
+
+module.exports = mongoose.model('Link', LinkSchema);
+
--- app/models/user.js
+++ app/models/user.js
@@ -1,25 +1,32 @@
-var db = require('../config');
 var bcrypt = require('bcrypt-nodejs');
 var Promise = require('bluebird');
 
-var User = db.Model.extend({
-  tableName: 'users',
-  hasTimestamps: true,
-  initialize: function(){
-    this.on('creating', this.hashPassword);
-  },
-  comparePassword: function(attemptedPassword, callback) {
-    bcrypt.compare(attemptedPassword, this.get('password'), function(err, isMatch) {
-      callback(isMatch);
-    });
-  },
-  hashPassword: function(){
-    var cipher = Promise.promisify(bcrypt.hash);
-    return cipher(this.get('password'), null, null).bind(this)
-      .then(function(hash) {
-        this.set('password', hash);
-      });
-  }
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var UserSchema = new Schema({
+  username: { type: String, default: '' },
+  password: { type: String, default: '' }
 });
 
-module.exports = User;
+UserSchema.pre('save', function(next) {
+  var cipher = Promise.promisify(bcrypt.hash);
+
+  cipher(this.password, null, null).bind(this)
+    .then(function(hash) {
+      this.password = hash;
+      next();
+    });
+
+});
+
+UserSchema.methods = {
+  comparePassword: function(attemptedPassword, cb){
+    bcrypt.compare(attemptedPassword, this.password, function(err, isMatch) {
+      cb(isMatch);
+    });
+  }
+}
+
+module.exports = mongoose.model('User', UserSchema);
+
